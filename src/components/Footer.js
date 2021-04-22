05173743b5ecf18819bf4aefcd511fa85eb41846
Sam Scibetta <sammieeccentric@gmail.com>
2021-04-22 06:57:26 -0400

    corrected repo link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
 <footer>
   <div className="container">
     This website was built in 2021 using React, HTML, JavaScript, Bootstrap, Custom CSS Styling, Greensock Animations, and Adobe Illustrator. 
-    <a href="https://github.com/scibettas1/react-portfolio" target="_blank" rel="noreferrer"><button type="button" className="view-btn">View Repository</button></a>
+    <a href="https://scibettas1.github.io/" target="_blank" rel="noreferrer"><button type="button" className="view-btn">View Repository</button></a>
   </div>
 
 </footer>
